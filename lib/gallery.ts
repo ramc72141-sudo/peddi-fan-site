817441489e9a98244f3138d57ee7b76d3134f971
ramc72141-sudo <ramc72141@gmail.com>
2026-03-02 18:32:22 +0530

    Add new image to gallery

--- lib/gallery.ts
+++ lib/gallery.ts
@@ -37,4 +37,5 @@ export const galleryImages = [
   { id: 32, src: "/gallery/32.jpg" },
   { id: 33, src: "/gallery/33.jpg" },
   { id: 34, src: "/gallery/34.jpg" },
+  { id: 35, src: "/gallery/35.jpg" },
 ];
